9592c5366778baab62964fae2e0aaf9d217d499e
Guto Xavier <gutoffline@gmail.com>
2022-10-07 16:49:54 -0300

    encontro 02 - sintaxe, variaveis, tipos de dados, entrada e saída

--- 002/app.js
+++ 002/app.js
@@ -43,3 +43,32 @@ console.log(typeof carros)
 
 let pessoa = {'nome':'guto', 'idade':'36'}
 console.log(typeof pessoa)
+
+
+document.getElementById("titulo").style.backgroundColor = "lightgreen"
+console.log(document.getElementById("titulo"))
+document.getElementById("titulo").innerText = "Melhor Sabor"
+let tituloDaPagina
+tituloDaPagina = document.getElementById("titulo").innerText
+console.log(tituloDaPagina)
+
+let elementoSubtitulo
+elementoSubtitulo = document.getElementById("subtitulo")
+console.log(elementoSubtitulo)
+elementoSubtitulo.style.backgroundColor = "lightblue"
+elementoSubtitulo.innerHTML = "<em>Seu restaurante certo</em>"
+
+
+let frutas = ["🍉","🍋","🍒"]
+console.log(frutas)
+console.dir(frutas)
+console.table(frutas)
+
+document.write("<h3>Cardápio</h3>")
+document.write(frutas)
+
+alert("Bom dia, seja bem vindo ao meu site")
+let nomeCompleto = prompt("Qual seu nome?","Digite aqui")
+
+alert("Olá " + nomeCompleto + " espero que goste")
+document.write("<br>nome: " + nomeCompleto)
