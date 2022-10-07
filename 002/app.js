bc05686446e47dbd33ef2eed423f55d9fd8a2792
Guto Xavier <gutoffline@gmail.com>
2022-10-07 14:50:20 -0300

    trabalhando com variáveis

---
+++ 002/app.js
@@ -0,0 +1,27 @@
+console.log("Encontro 2")
+
+/*
+Sintaxe javascript, variáveis e tipos de dados
+*/
+
+// variáveis
+
+nome = "Guto"
+console.log(nome)
+var nome = "José"
+console.log(nome)
+var nome = "Silvia"
+console.log(nome)
+let sobrenome = "Xavier"
+console.log(sobrenome)
+
+if(10>3){
+    console.log("10 maior que 3")
+    let idade=21
+    console.log(idade)
+}
+
+//console.log(idade)
+
+let midia = "tv"
+console.log(Midia)
