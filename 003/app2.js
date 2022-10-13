27b5570ee44a323566ca5a362ceeab7931e98e67
Guto Xavier <gutoffline@gmail.com>
2022-10-13 16:16:50 -0300

    operadores lógicos

--- 003/app2.js
+++ 003/app2.js
@@ -31,3 +31,35 @@ let n3 = "10"
 document.write((n1 == n3) + "<br>")
 document.write((n1 === n3))
 
+
+/* operadores lógicos
+ && -->  AND (e)
+ Todas as expressões devem ser verdadeiros para retornar verdadeiro
+
+ 10 > 5 --> TRUE
+ 10 > 20 --> FALSE
+ 10 > 5 && 10 > 20 --> FALSE
+*/
+document.write("<br><br><br>")
+document.write(10>5 && 10>20)
+document.write("<br>")
+
+/*
+|| --> OR (ou)
+Se uma expressão for verdadeira, o retorno é verdadeiro
+*/
+document.write(10>5 || 10>20)
+document.write("<br>")
+/*
+
+! --> NOT (não)
+inverte o resultado
+*/
+document.write(!true)
+document.write("<br>")
+document.write(10>5)
+document.write("<br>")
+document.write(!10>5)
+document.write("<br>")
+document.write(10+5 > 3*3)
+
